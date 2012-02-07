9a405a4e025117aa9ec08c9067ceeae15c9f3265
Sean Gaffney <seangaffney@gmail.com>
2012-02-07 16:21:34 -0500

    Handle documents with no meta-data

--- models/document_parser.rb
+++ models/document_parser.rb
@@ -3,9 +3,14 @@ require 'yaml'
 class DocumentParser
 
   def self.parse(contents)
-    meta, markup = contents.split(/\r?\n---+\r?\n/, 2)
-    meta = YAML.load(meta)
-    metadata = Hashie::Mash.new(meta)
+    if contents.include?('---') && contents[0..25].include?(':')
+      meta, markup = contents.split(/\r?\n---+\r?\n/, 2)
+      meta = YAML.load(meta)
+      metadata = Hashie::Mash.new(meta)
+    else
+      markup = contents
+      metadata = Hashie::Mash.new
+    end
     
     return markup, metadata
   end
