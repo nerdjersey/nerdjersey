d20ad96dc40b4323bd7517afd713706a606453eb
Sean Gaffney <seangaffney@gmail.com>
2012-04-11 18:31:55 -0400

    New example config files

--- config/dropbox-example.rb
+++
@@ -1,9 +0,0 @@
-Dropbox::API::Config.app_key    = 'AAAAAAAAAAAAAAA'
-Dropbox::API::Config.app_secret = 'BBBBBBBBBBBBBBB'
-Dropbox::API::Config.mode       = 'sandbox'
-
-DROPBOX_CLIENT_TOKEN = 'XXXXXXXXXXXXXXX'
-DROPBOX_CLIENT_SECRET = 'YYYYYYYYYYYYYYY'
-
-# For debuggin in irb or pry, set up a client with this command
-# client = Dropbox::API::Client.new(:token  => 'XXXXXXXXXXXXXXX', :secret => 'YYYYYYYYYYYYYYY')
